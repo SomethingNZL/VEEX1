047ba2dd1282d637d9623ff3dce17c97edbfe863
SomethingNZL <lup062886@gmail.com>
2026-04-08 01:24:32 +1200

    Remove redundant Engine ShaderKit folder

--- Engine ShaderKit/src copy 2/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "veex/Application.h"
-
-int main(int argc, char** argv) {
-    veex::Application app("Game/gameinfo.txt", "");
-    return app.Run();
-}
--- Engine ShaderKit/src copy 3/PropStaticEntity.cpp
+++
@@ -1,7 +0,0 @@
-#include "veex/PropStaticEntity.h"
-
-namespace veex {
-
-VEEX_REGISTER_ENTITY("prop_static", PropStaticEntity);
-
-} // namespace veex
